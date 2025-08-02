2c2f6497ee6b7ef44e945ee97450e32281097e8f
esamkido1994 <143332759+esamkido1994@users.noreply.github.com>
2025-08-02 12:09:33 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// تقديم الملفات الثابتة من مجلد public (نحتاج تنقل ملف html هنا)
-app.use(express.static(path.join(__dirname, 'public')));
+// توفير الملفات الثابتة (html, css, js)
+app.use(express.static(path.join(__dirname)));
 
-// إذا فتحت الروت الأساسي يرسل ملف index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// جميع الطلبات ترجع index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
